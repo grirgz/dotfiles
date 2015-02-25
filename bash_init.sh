44cdc86bbdf537ee966274d194f7a9cdcf5cd48e
grirgz <grirgz.ns@gmail.com>
2015-02-25 17:08:54 +0100

    bash

--- bash_init.sh
+++ bash_init.sh
@@ -3,5 +3,6 @@ seco=~/.local/share/SuperCollider/Extensions/seco/seco
 Seco=~/.local/share/SuperCollider/Extensions/seco/
 live=~/code/sc/seco/vlive
 cuspn=~/code/sc/seco/vlive/cuspn
+quarks=~/.local/share/SuperCollider/Extensions/
 
 export seco Seco live cuspn
