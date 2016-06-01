302ea10f71b1eb233cc05a590b24af1446da3a3f
grirgz <grirgz.ns@gmail.com>
2016-06-01 14:32:17 +0200

    add myrepos

---
+++ system_init.sh
@@ -0,0 +1,10 @@
+sudo apt-get install build-essential vim-gnome screen htop cmake-curses-gui
+sudo apt-get install jackd2
+
+# Supercollider
+
+sudo apt-get install pulseaudio-module-jack && pkill pulseaudio
+sudo apt-get install ssh open-sshd
+sudo apt-get install myrepos
+
+sudo apt-get install build-essential libjack-jackd2-dev libsndfile1-dev libasound2-dev libavahi-client-dev libicu-dev libreadline6-dev libfftw3-dev libxt-dev libudev-dev libcwiid-dev pkg-config git cmake qt5-default qt5-qmake qttools5-dev qttools5-dev-tools qtdeclarative5-dev libqt5webkit5-dev qtpositioning5-dev libqt5sensors5-dev
